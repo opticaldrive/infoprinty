5c5d9e47122f4376030522f5f630a6d156140cd8
aaaaa <138826219+opticaldrive@users.noreply.github.com>
2026-06-24 11:35:09 -0700

    fix: use x-forwaderde for header

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root(request: Request):
-    return {"ip": request.client.host}
+    return {
+        "request.client.host": request.client.host,
+        "ip": request.headers.get("X-Forwarded-For"),
+    }
