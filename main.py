60606c6043e37a6f475be0edfd6ee4be0b1f81b4
aaaaa <138826219+opticaldrive@users.noreply.github.com>
2026-06-25 02:00:46 -0700

    feat: ip to mappy (ts was so buggy) help

--- main.py
+++ main.py
@@ -1,39 +1,36 @@
 from fastapi import FastAPI, Request
-
-app = FastAPI()
-
-
-# @app.get("/")
-# async def root(request: Request):
-#     return {
-#         "request.client.host": request.client.host,
-#         "ip": request.headers.get("X-Forwarded-For"),
-#     }
-
 from fastapi import Request, APIRouter
 from fastapi.templating import Jinja2Templates
 
 
-# https://fastapi.tiangolo.com/tutorial/bigger-applications/#import-apirouter
+app = FastAPI()
 
-router = APIRouter(tags=["pages"])
 
 templates = Jinja2Templates(
     directory="templates/"
-)  # todo -  move everything to template
-
+)  
 
 @app.get("/")
-def get_homepage(request: Request):  # sync = runs in threadpool,
-    # so its blocking DB work never stalls the event loop (matters under HN-spike load)
+def get_homepage(request: Request): 
     return templates.TemplateResponse(
         request=request,
         name="index.html",
         context={
             "request.client.host": request.client.host,
             "ip": request.headers.get("X-Forwarded-For"),
+            "headers": request.headers,
+            "cookies": request.cookies
         },
     )
 
+@app.get("/headers")
+def get_headers(request: Request):  
+    return request.headers
 
-# ui views tbd
+@app.get("/cookies")
+def get_headers(request: Request):  
+    return request.cookies
+
+
+
+
