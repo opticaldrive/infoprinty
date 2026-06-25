eae38b430edfe1040e64c3b1b99539890b0d723b
aaaaa <138826219+opticaldrive@users.noreply.github.com>
2026-06-25 02:41:55 -0700

    :3

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi import Request, APIRouter
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 templates = Jinja2Templates(
